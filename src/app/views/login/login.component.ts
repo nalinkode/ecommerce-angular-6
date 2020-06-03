9f124d72f79329c36aadee6a435b1e3b81b52bfc
nalinkode <nalinkode35@gmail.com>
2020-06-04 02:18:32 +0530

    1 file modified

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -44,7 +44,8 @@ export class LoginComponent implements OnInit {
       }
       const md5 = new Md5();
       const pwd = md5.appendStr(this.loginForm.value.password).end();
-      const dat = { 'Email': this.loginForm.value.email, 'Password': pwd , 'RememberMe': this.rememberMe };
+      const dat = { 'Email': this.loginForm.value.email, 'Password': pwd , 
+       'RememberMe': this.loginForm.value.rememberMe };
       console.log(dat);
 
      }

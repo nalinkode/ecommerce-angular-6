6c7aa46dcf3df0cf161b3897796cb1167cbc7cb6
nalinkode <nalinkode35@gmail.com>
2020-06-04 14:54:21 +0530

    2 files modified

--- src/app/views/login/login.service.ts
+++ src/app/views/login/login.service.ts
@@ -3,12 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(data){
+  return this.http.post(environment.BASE_URL + 'authenticate', data).pipe(map(res => res));
+  }
+
+
+
+
 
 }

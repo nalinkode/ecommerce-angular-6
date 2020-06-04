1f66953e7daf064553b6fe74a5d87dc14c17c66c
nalinkode <nalinkode35@gmail.com>
2020-06-04 22:21:21 +0530

    1 file modified

--- src/app/views/auth/auth.guard.ts
+++ src/app/views/auth/auth.guard.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
+   constructor( private router : Router ){}
+  
+   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+
+    const TRUE = 'true';  
+    const isloggedIn = Cookie.get('isLoggedIn');
+  
+    if(isloggedIn === TRUE) {
+      return true;
+    } 
+    this.router.navigate(['login']);
+    return false;
   }
 }

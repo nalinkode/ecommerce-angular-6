2d8453271dec2d560eac49fc820aeceea25214b1
nalinkode <nalinkode35@gmail.com>
2020-06-04 23:57:33 +0530

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {}
+export class AppComponent  {
+// Wires up BlockUI instance
+  @BlockUI() blockUI: NgBlockUI;
+
+  
+}
 

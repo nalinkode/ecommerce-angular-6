8dacd4b2a243e3b3931c0603635187d99bcae2e4
nalinkode <nalinkode35@gmail.com>
2020-06-04 21:46:24 +0530

    1 file modified

--- src/app/views/auth/auth-interceptor.service.ts
+++ src/app/views/auth/auth-interceptor.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
 
 @Injectable()
-export class AuthInterceptorService {
+export class AuthInterceptorService implements HttpInterceptor {
+intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = Cookie.get('token'); // you probably want to store it in localStorage or something
 
-  constructor() { }
+    if (!token) {
+      return next.handle(req);
+    }
+       return next.handle(req);
+    }
+
 
 }

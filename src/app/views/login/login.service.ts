c013f6b6ef57dba1d84f55d033ec0b495a19ad33
nalinkode <nalinkode35@gmail.com>
2020-06-10 15:18:15 +0530

    1 file modified

--- src/app/views/login/login.service.ts
+++ src/app/views/login/login.service.ts
@@ -13,7 +13,7 @@ export class LoginService {
 
   login(data){
   debugger
-    return this.http.post('/assets/users.json',data).pipe(map(res => res));
+    return this.http.post('/assets/user.json',data).pipe(map(res => res));
   }
 
 

3b3be97834356e2f79b16d8530c0e549f02704e6
nalinkode <nalinkode35@gmail.com>
2020-06-15 18:32:58 +0530

    1 file modified

--- src/app/admin/product/product.component.ts
+++ src/app/admin/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 

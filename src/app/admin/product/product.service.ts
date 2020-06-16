070f13b01a57776873bc27950bc055e7c6e1898d
nalinkode <nalinkode35@gmail.com>
2020-06-16 14:55:51 +0530

    1 file modified

--- src/app/admin/product/product.service.ts
+++ src/app/admin/product/product.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +10,7 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  public getProduct(){
+  public getAllProduct(){
     return this.http.get('https://my-json-server.typicode.com/jubs16/Products/Products');
   }
 

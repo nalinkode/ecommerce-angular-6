192a55d71f9694fb45a30bef16f5518060b8d47d
nalinkode <nalinkode35@gmail.com>
2020-06-19 13:44:43 +0530

    1 file modified

--- src/app/admin/product/product.component.ts
+++ src/app/admin/product/product.component.ts
@@ -4,6 +4,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
 import { ProductService } from '../product/product.service';
+import { DialogboxComponent } from '../dialogbox/dialogbox.component';
 
 
 export interface Product {
@@ -44,6 +45,48 @@ export class ProductComponent implements OnInit {
    public applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase(); 
   }
+  
+  openDialog(action,obj) {
+    obj.action = action;
+    const dialogRef = this.dialog.open(DialogboxComponent, {
+      width: '250px',
+      data:obj
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result.event == 'Add'){
+        this.addRowData(result.data);
+      }else if(result.event == 'Update'){
+        this.updateRowData(result.data);
+      }else if(result.event == 'Delete'){
+        this.deleteRowData(result.data);
+      }
+    });
+  }
+
+  public addRowData(row_obj){
+    var d = new Date();
+    this.dataSource.push({
+      id:d.getTime(),
+      name:row_obj.name
+    });
+    this.table.renderRows();
+    
+  }
+  public updateRowData(row_obj){
+    this.dataSource = this.dataSource.filter((value,key)=>{
+      if(value.id == row_obj.id){
+        value.name = row_obj.name;
+      }
+      return true;
+    });
+  }
+  public deleteRowData(row_obj){
+    this.dataSource = this.dataSource.filter((value,key)=>{
+      return value.id != row_obj.id;
+    });
+  }
+ 
 
 }
 

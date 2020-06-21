17b4bd893b95c63c4cda1b0d54a5eda91d3f1add
nalinkode <nalinkode35@gmail.com>
2020-06-21 13:45:09 +0530

    2 files modified

--- src/app/admin/product/product.component.ts
+++ src/app/admin/product/product.component.ts
@@ -15,7 +15,7 @@ import { AddProductComponent } from './add-product/add-product.component';
 
 export class ProductComponent implements OnInit {
   ELEMENT_DATA : Product[];
-  displayedColumns: string[] = ['productId','name','categroy','imgUrl','price','description', 'action'];
+  displayedColumns: string[] = ['productId', 'name',' categroy','subCategory','imgUrl', 'price', 'offerPrice', 'description', 'action'];
   dataSource = new MatTableDataSource<Product>(this.ELEMENT_DATA);
   
   isPopupOpen = false;

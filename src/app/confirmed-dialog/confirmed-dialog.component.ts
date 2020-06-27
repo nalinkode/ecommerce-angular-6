963f517e22768c6d8a7f7403c7b60ffd69f9782b
nalinkode <nalinkode35@gmail.com>
2020-06-27 19:50:55 +0530

    1 file modified

--- src/app/confirmed-dialog/confirmed-dialog.component.ts
+++ src/app/confirmed-dialog/confirmed-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirmed-dialog',
@@ -8,11 +8,13 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ConfirmedDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data,public dialogRef:MatDialogRef<ConfirmedDialogComponent>) { }
 
   ngOnInit() {
   }
 
-
+  closeDialog(){
+   this.dialogRef.close();
+  }
 
 }

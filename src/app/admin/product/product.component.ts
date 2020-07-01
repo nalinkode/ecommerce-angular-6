7ecd05eebf5640b812ede4e112108201ba8333bd
nalinkode <49398402+nalinkode@users.noreply.github.com>
2020-07-01 19:06:09 +0530

    Update product.component.ts

--- src/app/admin/product/product.component.ts
+++ src/app/admin/product/product.component.ts
@@ -1,17 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
-import { MatDialog } from '@angular/material/dialog';
-import { ProductService } from '../product/product.service';
-import { DialogService } from '../../shared/dialog.service';
-import { Product } from '../product/product';
-import { AddProductComponent } from './add-product/add-product.component';
-import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { ToastrManager } from 'ng6-toastr-notifications';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-
 
 @Component({
   selector: 'app-product',

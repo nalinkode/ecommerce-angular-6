9ca1068165e462eb18e0c9e541da402d5632daaf
nalinkode <49398402+nalinkode@users.noreply.github.com>
2020-07-01 23:19:15 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,25 @@ import { BlockUI, NgBlockUI } from 'ng-block-ui';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-// Wires up BlockUI instance
   @BlockUI('root') blockUI: NgBlockUI;
+
+  showHead: boolean = false;
+  showFooter: boolean = false;
   
-  constructor(){
+  constructor(private router : Router){
     this.blockUI.start();
     this.blockUI.stop();
+    router.events.forEach((event) => {
+      if(event instanceof NavigationStart) {
+        if(event['url'] == '/login') {
+         this.showHead = false;
+         this.showFooter = false;
+        } else {
+          this.showHead = true;
+          this.showFooter = true;
+        }
+      }
+    });
   }
-  
 }
 

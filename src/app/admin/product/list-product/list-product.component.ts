7faf675d287a225fcc6a57bddb08681a6be1d011
nalinkode <nalinkode35@gmail.com>
2020-07-01 18:12:40 +0530

    1 file modified

--- src/app/admin/product/list-product/list-product.component.ts
+++ src/app/admin/product/list-product/list-product.component.ts
@@ -3,11 +3,15 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
-
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { ToastrManager } from 'ng6-toastr-notifications';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+
+import { ProductService } from '../product.service';
+import { DialogService } from '../../../shared/dialog.service';
+import { Product } from '../product';
+import { AddProductComponent } from '../add-product/add-product.component';
 
 
 @Component({

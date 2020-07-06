64cc1ac60b9f70f456f8e30e67e2ab97e25f9163
nalinkode <49398402+nalinkode@users.noreply.github.com>
2020-07-06 21:54:22 +0530

    Update product.service.ts

--- src/app/admin/product/product.service.ts
+++ src/app/admin/product/product.service.ts
@@ -19,7 +19,7 @@ export class ProductService {
 
 
   public getProductById(productId : number): Observable<Product>{
-    return this.http.get('/assets/product.json');
+    return this.http.get('/assets/productbyid.json');
   }
   
   public addProduct(product:Product){

1f0e538bb32e26ec2de066131446300e1c740c35
nalinkode <49398402+nalinkode@users.noreply.github.com>
2020-07-09 23:48:17 +0530

    Update category-list.component.ts

--- src/app/admin/category/category-list/category-list.component.ts
+++ src/app/admin/category/category-list/category-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { ToastrManager } from 'ng6-toastr-notifications';
+import { Router } from '@angular/router';
+import { DialogService } from '../../../shared/dialog.service';
+import { Category } from '../../../shared/category';
 
 @Component({
   selector: 'app-category-list',
@@ -7,7 +16,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  @BlockUI() blockUI: NgBlockUI;
+
+  ELEMENT_DATA : Category[];
+  displayedColumns: string[] = ['serialNumber','category', 'status', 'action'];
+  dataSource = new MatTableDataSource<Category>(this.ELEMENT_DATA);
+  
+  isPopupOpen = false;
+  isLoading = true;
+
+  constructor(
+             private dialogService : DialogService,
+             private router: Router,
+             private toastr : ToastrManager,     
+             private dialog? : MatDialog) { }
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
 
   ngOnInit() {
   }

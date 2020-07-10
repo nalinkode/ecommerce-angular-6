cc33de97cb5833f24dc9c34f0fbdc70bc751cb3d
nalinkode <nalinkode35@gmail.com>
2020-07-10 15:42:03 +0530

    1 file added

--- src/app/admin/category/category-dialog/category-dialog.component.ts
+++ src/app/admin/category/category-dialog/category-dialog.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrManager } from 'ng6-toastr-notifications';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { CategoryService } from '../../../shared/category.service';
 
 @Component({
   selector: 'app-category-dialog',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-dialog.component.css']
 })
 export class CategoryDialogComponent implements OnInit {
-
-  constructor() { }
+  categoryForm : FormGroup; 
+  @BlockUI() blockUI: NgBlockUI;
+  constructor(private fb: FormBuilder,
+              private categoryService: CategoryService, 
+              private toaster: ToastrManager, 
+              private http: HttpClientModule
+              ) { }
 
   ngOnInit() {
+      this.createCategoryForm();
+  }
+
+  createCategoryForm(){
+     this.categoryForm = this.fb.group({
+     category: ['', Validators.required], 
+     subCategory: ['', Validators.required],
+     isActivated:[tr]
+   });
   }
 
 }

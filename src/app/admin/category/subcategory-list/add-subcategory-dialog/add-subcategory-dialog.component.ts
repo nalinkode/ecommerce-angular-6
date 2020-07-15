0cbef0f4e62be012733a541468fbcfb96fdc6268
nalinkode <49398402+nalinkode@users.noreply.github.com>
2020-07-15 19:27:39 +0530

    Update add-subcategory-dialog.component.ts

--- src/app/admin/category/subcategory-list/add-subcategory-dialog/add-subcategory-dialog.component.ts
+++ src/app/admin/category/subcategory-list/add-subcategory-dialog/add-subcategory-dialog.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrManager } from 'ng6-toastr-notifications';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { CategoryService } from '../../../../shared/category.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-subcategory-dialog',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-subcategory-dialog.component.css']
 })
 export class AddSubcategoryDialogComponent implements OnInit {
+  
+  subCategoryForm : FormGroup; 
+  @BlockUI() blockUI: NgBlockUI;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private categoryService: CategoryService, 
+              private toaster: ToastrManager, 
+              private http: HttpClientModule,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private dialogRef : MatDialogRef<AddSubcategoryDialogComponent>) { }
 
   ngOnInit() {
+     this.createSubCategoryForm();
   }
+
+  createSubCategoryForm(){
+     this.subCategoryForm = this.fb.group({
+     category: ['', Validators.required], 
+     isActivate:['', Validators.required]
+   });
+  }
+
+  onNoClick(){
+    this.dialogRef.close();
+  }
+
+  onSubmit(){
+    //console.log(this.categoryForm.value);
+    this.toaster.successToastr('Sub category added successfully .');  
+    this.dialogRef.close();
+  }
+
 
 }

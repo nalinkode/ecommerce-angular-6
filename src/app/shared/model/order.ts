931a2305021877f27b9980f58d7ad2ca87a3407d
nalinkode <nalinkode35@gmail.com>
2020-07-17 16:04:33 +0530

    2 files modified

--- src/app/shared/model/order.ts
+++ src/app/shared/model/order.ts
@@ -1,2 +1,28 @@
 export interface Order {
+  orderLineId : number;
+  quantity : number;
+  order : IOrder;
+  product : IProduct;
+}
+
+interface IOrder {
+ orderId: number;
+ orderDate: String;
+ total : number;
+ isOrderCompleted: string;
+ customer : Customer;
+}
+
+interface Customer {
+   customerId: number;
+   name : string;
+   email : string; 
+   phone : number;
+}
+
+interface IProduct {
+    productId  : number;
+    productName :string;
+    price : number;     
+    offerPrice : number;
 }

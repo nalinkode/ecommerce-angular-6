cb14cea2ac78edcf79302fda88193d190626d1a1
nalinkode <nalinkode35@gmail.com>
2020-07-23 20:37:24 +0530

    2 files modified

--- src/app/views/login/reset-password/reset-password.component.ts
+++ src/app/views/login/reset-password/reset-password.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { ToastrManager } from 'ng6-toastr-notifications';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  resetPasswordForm : FormGroup; 
+  @BlockUI() blockUI: NgBlockUI;
+  
+  constructor(private fb: FormBuilder,private loginService: LoginService, private router: Router,private http: HttpClientModule,private toaster: ToastrManager) { }
 
   ngOnInit() {
+    this.createResetPasswordForm()
+  }
+
+  createResetPasswordForm(){
+    this.resetPasswordForm = this.fb.group({
+     newPassword: ['', Validators.required],
+     confirmedPassword: ['', Validators.required]
+   });
   }
 
 }
